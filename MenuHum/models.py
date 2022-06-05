8de05d8e6bb968a731fec8b71ca6a1cf656162f0
Damian Juarez <damianjuarez@hotmail.com.ar>
2022-06-04 23:38:11 -0300

    Formulario y menu H listo

--- MenuHum/models.py
+++ MenuHum/models.py
@@ -6,4 +6,4 @@ class Menu_Hum(models.Model):
     producto = models.CharField(max_length=30)
     precio = models.FloatField()
     detalle = models.CharField(max_length=100)
-   
+    active = models.BooleanField(default=True)
